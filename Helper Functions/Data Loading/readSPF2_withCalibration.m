6c054fad745588b292ff834108f61371610a4ab9
neerja <neerja.aggarwal42@gmail.com>
2022-07-13 13:38:43 -0700

    cleaned up the way we import ground truth spectrum. Added recon code for using the gauss SVD method.

--- Helper Functions/Data Loading/readSPF2_withCalibration.m
+++ Helper Functions/Data Loading/readSPF2_withCalibration.m
@@ -1,14 +1,18 @@
-function [spectrum,wavelength] = readSPF2_withCalibration(calibrationCurve,...
-    calibrationOrig,signalRange,filename)
+% readSPF2_withCalibration.m
+% last argument (wavelengthInterp) is optional. It will resample the
+% spectrum after calibration correction. 
+
+function [spectrum,wavelength] = readSPF2_withCalibration(wavelengthOrig,...
+    wavelengthCorrected,signalRange,filename1,wavelengthInterp)
 
     %if no filename is supplied, open the GUI to select the file
-    if isempty(filename)
+    if nargin<4
         disp('Select ground truth for measurement')
         [spf2File,path] = uigetfile('.spf2');
-        filename = [path,spf2File]
+        filename1 = [path,spf2File]
     end
     
-    data = readSPF2(filename);
+    data = readSPF2(filename1);
     
     % if signalRange isn't specified, use default
     if isempty(signalRange)
@@ -17,7 +21,14 @@ function [spectrum,wavelength] = readSPF2_withCalibration(calibrationCurve,...
     
     wavelength = data(signalRange,1); %get subset of spectrometer input
     
-    %apply correction spectrometer output and sample at calibration
-    %wavelengths?  %ASK JOE
-    spectrum = interp1(calibrationCurve,data(signalRange,2),calibrationOrig);
+    %apply correction spectrometer output and sample at calibration (2 nm
+    %shift)
+    spectrum = interp1(wavelengthOrig,data(signalRange,2),wavelengthCorrected);
+
+    if nargin<5
+        %do nothing
+    else
+        spectrum = interp1(wavelength,spectrum,wavelengthInterp);
+        wavelength = wavelengthInterp;
+    end
 end
