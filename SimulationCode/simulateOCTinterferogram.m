071216a40ff8d1b8752733cdfa5f3ce2e13e371b
Joe Malone <joseph.d.malone@vanderbilt.edu>
2022-09-20 09:55:06 -0500

    adding simulation code
    main simulation code is simulateDiffuserSpectrum_wNoise

---
+++ SimulationCode/simulateOCTinterferogram.m
@@ -0,0 +1,43 @@
+function [interferogram] = simulateOCTinterferogram(wavelength, intensityWavlength, numberReflectors, depth)
+
+    %% Linearize spectrum with respect to wavenumber
+    wavenumber = 2*pi./wavelength;
+    wavnumberLinear = linspace(max(wavenumber),min(wavenumber),numel(wavelength));
+    deltaK = wavnumberLinear(1) - wavnumberLinear(2);
+
+    % Resample the spectrum
+    intensityWavenumber = interp1(wavenumber, intensityWavlength, wavnumberLinear);
+
+    %% Simulate the interferogram 
+    
+    numLayer = size(numberReflectors);
+    reflector_signal = 0;
+
+    % Initialize the frequency response function
+    H = 0;
+    
+    for layer = 1:numLayer(2)-1
+
+        % Calculate the refelection coefficient 
+        reflector_n = numberReflectors(layer+1)-(numberReflectors(layer))/...
+                    (numberReflectors(layer+1)+numberReflectors(layer));
+
+
+        reflector_signal = reflector_signal + numberReflectors(layer)...
+                    * (depth(layer+1) - depth(layer));
+        H = H + reflector_n*exp(1i*2.*wavnumberLinear*reflector_signal);
+
+    end
+    
+    % Calculate the cross-crorelation term
+    interferogram = zeros(1,numel(wavelength));
+    
+    for layer=1:length(wavnumberLinear)
+
+        interferogram(layer) = real(intensityWavenumber(layer)...
+                    *H(layer));
+
+    end
+
+end
+
